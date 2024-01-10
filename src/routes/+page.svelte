3ed927ce886f3bc5f617ecb200766e1f9461588b
hashtoes <hashtoes@gmail.com>
2024-01-10 17:29:57 +0900

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let numLanes = 28;
+</script>
+
+<div class="full-height">
+  <div class="lane-pane">
+    {#each Array(numLanes).fill(0) as idx}
+      <div>hello</div>
+    {/each}
+  </div>
+  <div class="input-pane"></div>
+</div>
+
+<style>
+	.full-height {
+		height: 100vh;
+	}
+  .lane-pane {
+    height: 80vh;
+    overflow-y: scroll;
+  }
+  .input-pane {
+    height: 20vh;
+    border-top: 1px solid;
+    box-sizing: border-box;
+  }
+</style>
