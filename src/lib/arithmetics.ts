4c533a73d6410e93191b77553166eeb59d8b16db
hashtoes <hashtoes@gmail.com>
2024-01-20 16:17:15 +0900

    arith tweak

--- src/lib/arithmetics.ts
+++ src/lib/arithmetics.ts
@@ -3,7 +3,7 @@ type Problem = {
   answer: number;
 };
 
-const MAX_VALUE = 100;
+const MAX_VALUE = 99;
 
 const operators = [
   {
@@ -40,7 +40,7 @@ const operators = [
     symbol: '/',
     forward: (op1: number, op2: number) => op1 / op2,
     backward: (answer: number, op2: number) => answer * op2,
-    maxOp2: (answer: number) => 20, // to adjust the difficulty
+    maxOp2: (answer: number) => 9, // to adjust the difficulty
     adjust: (tempOp1: number, op2: number, tempAnswer: number) => ({
       op1: tempOp1,
       answer: tempAnswer
@@ -51,7 +51,7 @@ const operators = [
 export const generateProblem = (): Problem => {
   const operator = operators[Math.floor(Math.random() * operators.length)];
 
-  const tempAnswer = Math.floor(Math.random() * MAX_VALUE);
+  const tempAnswer = Math.floor(Math.random() * MAX_VALUE) + 1;
   const op2 = Math.floor(Math.random() * operator.maxOp2(tempAnswer)) + 1;
   const tempOp1 = operator.backward(tempAnswer, op2);
 
